ee3069810df25f83b28e14542cea59bee08bc58e
Joe Bucciero <jbucciero@ymail.com>
2021-01-27 20:43:51 -0500

    Adding author name

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,5 +1,5 @@
 // Simple C++ program to display "Hello World" 
-// Header file for input output functions 
+// Author: Joe Bucciero <wayne state email: gj4652@wayne.edu>
 #include<iostream>  
   
 using namespace std; 
